ebc3cda0e46717f12f7b521810f8e9a7a05d8961
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-11-11 15:55:23 +0100

    Updating Migrations

--- Impf_App/Data/Migrations/20211111144001_UpdatingVaccines.Designer.cs
+++ Impf_App/Data/Migrations/20211111144001_UpdatingVaccines.Designer.cs
@@ -4,14 +4,16 @@ using Impf_App.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Impf_App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211111144001_UpdatingVaccines")]
+    partial class UpdatingVaccines
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
