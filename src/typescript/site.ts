a492a919d37ab10b079fdfe0bbf889d46e33bca5
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-11-11 17:46:30 +0100

    adding sass and  ts

--- src/typescript/site.ts
+++ src/typescript/site.ts
@@ -1,4 +1,4 @@
-"use strict";
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+
 // Write your TypeScript code.
